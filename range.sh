bb8872d4a108ceaf013e147de23359eb5ce5cf2b
root <root@Mastermind>
2020-07-26 17:48:03 -0300

    I add a Head above the script, content the some informations. The principal information is the Goals. The Goals of this repository is create script to become commands or a softwares in bash. So if you want to help, you are welcome!

--- range.sh
+++ range.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+##########################################################
+#
+# Name: range.sh
+# Author: Ícaro César
+# Version: 1.0
+#
+# Goals: Hello Friend. My goal with each script
+# is to make it a command or program to be used on
+# Linux at the end of its process. So I thank anyone
+# who wants to make guesses.
+#
+#
+##########################################################
+
+
 if ["$1" == ""]
 then
 echo -e "\n This scripts need a object!\n\nExemple: 192.168.0\n"
